00e5ae55e3e7634006c81e4ac5fa0a8c88e08bef
Gustavo Henrick <gustavo08henrick@gmail.com>
2021-05-16 20:13:09 -0300

    FIrst Commit

---
+++ chatbot/lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 5,
+        backgroundColor: Colors.yellow,
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
+        ],
+      ),
+    );
+  }
+}
